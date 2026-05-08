d97bd1a5b7c193ce869b010bc85ead2f5c40e0cf
eaves-shaun <95596051+eaves-shaun@users.noreply.github.com>
2026-05-08 22:02:26 +1200

    Reduce logo size in App component

--- src/App.jsx
+++ src/App.jsx
@@ -626,7 +626,7 @@ export default function NZERA5DashboardPrototype() {
             <img
               src={`${APP_BASE_URL}NZClimTracker-logo-noText.png`}
               alt="NZ Climate Tracker logo"
-              className="w-8 h-8 md:w-10 md:h-10 object-contain mt-1"
+              className="w-2 h-2 md:w-4 md:h-4 object-contain mt-1"
             />
           
             <div>
